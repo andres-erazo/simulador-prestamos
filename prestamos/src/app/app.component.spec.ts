57165f3de4b94d43fb9e318204c9aeaa2da729e8
aerazo <aerazo@bancolombia.com.co>
2022-06-29 19:14:19 -0500

    cambio de nombre

--- prestamos/src/app/app.component.spec.ts
+++ prestamos/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prestamos'`, () => {
+  it(`should have as title 'sistema-prestamos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prestamos');
+    expect(app.title).toEqual('sistema-prestamos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prestamos app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sistema-prestamos app is running!');
   });
 });
