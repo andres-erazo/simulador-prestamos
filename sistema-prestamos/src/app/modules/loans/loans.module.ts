537ee807f44c1a59d4a35e52355a3e437c938e40
aerazo <aerazo@bancolombia.com.co>
2022-06-29 19:47:47 -0500

    modules loan , users are added

---
+++ sistema-prestamos/src/app/modules/loans/loans.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoansComponent } from './pages/loans/loans.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoansComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoansModule { }
